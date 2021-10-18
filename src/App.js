e1e3ba72ab41bdbf39445af8574f9b3709dceaae
Phu Nguyen <ngp130895@gmail.com>
2021-10-18 16:51:26 +0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import './App.css';
-import Counter from './Counter';
-import React, { useState, createContext, useMemo } from 'react';
+import React, { createContext } from 'react';
+import CounterUseRef from './CounterUseRef';
 
 export const Context = createContext();
 
-const expensiveCalculation = (text) => {
-  console.log(text, "get called");
-  let now = performance.now();
-  // Artificial delay -- do nothing for 100ms
-  while (performance.now() - now < 100) { }
-  return text;
-};
-
 function App() {
-  const [countContext, setCountContext] = useState(0);
-  // only called once since dependency list is an empty array
-  const expensiveOnce = useMemo(() => expensiveCalculation('expensiveOnce'), []);
-  // called everytime countContext changes
-  const expensiveOnRendered = expensiveCalculation('expensiveOnRendered');
   return (
-    <Context.Provider value={{ countContext, setCountContext }}>
-      <p>{expensiveOnRendered}</p>
-      <p>{expensiveOnce}</p>
-      <Counter />
-    </Context.Provider>
+    <>
+      <CounterUseRef />
+    </>
   );
 }
 
---
+++ src/CounterUseRef.js
@@ -0,0 +1,27 @@
+
+import React, { useState, useRef, useEffect} from 'react';
+
+
+function CounterUseRef() {
+  const [count, setCount] = useState(0);
+  const inputRef = useRef();
+  const countRef = useRef(0);
+  useEffect(()=>{
+    console.log('countRef', countRef.current);
+  });
+  return (
+    <>
+      <h1>Counter Input</h1>  
+      <p>{count}</p>
+      <input ref={inputRef} />
+      <button onClick={() => { setCount(count + Number(inputRef.current.value)) }}>
+        Increment
+      </button>
+      <button onClick={() => {countRef.current++}}>
+        Increase count ref
+      </button>
+    </>
+  );
+}
+
+export default CounterUseRef;

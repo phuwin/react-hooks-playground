6e440ef4c3e7c15ed0874b260f409597d8b13f0f
Phu Nguyen <ngp130895@gmail.com>
2021-10-18 16:39:49 +0300

    useContext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
+import React, { useState, createContext } from 'react';
+export const Context = createContext();
 
 function App() {
+  const [countContext, setCountContext] = useState(0);
   return (
+    <Context.Provider value={{ countContext, setCountContext }}>
       <Counter />
+    </Context.Provider>
   );
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,15 +1,22 @@
-import { useState } from 'react';
-import useEffectExamples from './useEffectExamples';
+
+import { useState, useContext } from 'react';
+import { Context } from './App';
+import useEffectExample from './useEffectExamples';
 
 function Counter() {
   const [count, setCount] = useState(0);
-  useEffectExamples(count);
+  const { countContext, setCountContext } = useContext(Context);
+  useEffectExample(count);
   return (
     <>
       <h1>Counter</h1>  
+      <p>Count Context:{countContext}</p>
       <p>{count}</p>
       <button onClick={() => { setCount(count + 1) }}>
-        Increment
+        Increment local
+      </button>
+      <button onClick={() => { setCountContext(countContext + 1) }}>
+        Increment context
       </button>
     </>
   );

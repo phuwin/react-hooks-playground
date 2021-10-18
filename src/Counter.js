5232a46fc716a693aa8601603c054830b012463e
Phu Nguyen <ngp130895@gmail.com>
2021-10-18 16:31:48 +0300

    useState

---
+++ src/Counter.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h1>Counter</h1>  
+      <p>{count}</p>
+      <button onClick={() => { setCount(count + 1) }}>
+        Increment
+      </button>
+    </>
+  );
+}
+
+export default Counter;
